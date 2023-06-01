44ec9ea205fcfbdd27c0d54644c4e3391d020567
danish9797 <126759261+danish9797@users.noreply.github.com>
2023-06-01 11:00:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import streamlit as st
 
 # Set the URL of the Wikipedia page you want to scrape
 url = 'https://en.wikipedia.org/wiki/OpenAI'
@@ -12,8 +11,12 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 # Find and extract specific elements from the HTML
-title = soup.find('h1', class_='firstHeading').text.strip()
-summary = soup.find('div').p.text.strip()
+title_element = soup.find('h1')
+summary_element = soup.find('div', attrs={'id': 'mw-content-text'}).find('p')
+
+# Extract the text from the elements
+title = title_element.text.strip() if title_element else 'Title not found'
+summary = summary_element.text.strip() if summary_element else 'Summary not found'
 
 # Print the extracted data
 print('Title:', title)
