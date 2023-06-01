e6f8c9378f7a4cb24425997dfd59faa097a72df5
danish9797 <126759261+danish9797@users.noreply.github.com>
2023-06-01 10:55:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import streamlit as st
 
 # Set the URL of the page you want to scrape
 url = 'https://www.daraz.pk/'
@@ -12,12 +13,14 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 # Find and extract specific elements from the HTML
 product_elements = soup.find_all('div', class_='c2prKC')
+
+# Create a Streamlit table to display the scraped data
+table_data = []
 for product in product_elements:
     name = product.find('a', class_='c16H9d').text.strip()
     price = product.find('div', class_='c3gUW0').text.strip()
     rating = product.find('div', class_='c15YQ9').text.strip()
 
-    print('Name:', name)
-    print('Price:', price)
-    print('Rating:', rating)
-    print('---')
+    table_data.append([name, price, rating])
+
+st.table(table_data)
